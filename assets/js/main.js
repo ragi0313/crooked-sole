6664df70540488048148e9f6aa7be10ece2ce984
ragi0313 <montalboragii13@gmail.com>
2023-12-12 19:42:32 +0800

    change

---
+++ assets/js/main.js
@@ -0,0 +1,267 @@
+import { heroSlideData, saleData, productSideBannerImages, gridProducts } from "./productData.js";
+
+
+$(function () {
+  
+   //nav
+   const mobileMenuClose = function () {
+     $(".mobile-navigation-menu").removeClass("active");
+     $(".overlay").removeClass("active");
+   }
+ 
+   $(".action-btn").each(function () {
+     $(this).on("click", function () {
+       $(".mobile-navigation-menu").addClass("active");
+       $(".overlay").addClass("active");
+     });
+     $(".menu-close-btn").on("click", mobileMenuClose);
+     $(".overlay").on("click", mobileMenuClose);
+   });
+ 
+   //categories
+   $(".accordion-menu").each(function () {
+     $(this).on("click", function () {
+       var clickedBtn = $(this).next().hasClass("active");
+       $(".submenu-category-list").each(function () {
+         if (clickedBtn) return false;
+ 
+         if ($(this).hasClass("active")) {
+           $(this).removeClass("active");
+           $(".accordion-menu").removeClass("active");
+         }
+       });
+       $(this).next().toggleClass('active');
+       $(this).toggleClass('active');
+     });
+   });
+
+ 
+  //hero
+  $('.hero').html(`
+    <div class="container">
+      <div class="swiper-container">
+         <div class="swiper-wrapper">
+           ${heroSlideData.map((item) => `
+             <div class="swiper-slide">
+               <img src="${item.imgSrc}" alt="Product Banner" class="hero-img">
+               ${item.title ? `
+                 <div class="hero-content">
+                   <h2 class="hero-title" style="color: ${item.titleColor || 'inherit'}">${item.title}</h2>
+                   <p class="hero-text" style="color: ${item.textColor || 'inherit'}">${item.text}</p>
+                   <a href="shop.html" class="btn ${item.buttonClass || ''}">
+                     <span>Shop Now</span>
+                     <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
+                   </a>
+                 </div>
+               ` : ''}
+             </div>
+           `).join('')}
+         </div>
+         <div class="swiper-pagination"></div>
+         <div class="swiper-button-next"></div>
+         <div class="swiper-button-prev"></div>
+      </div>
+    </div>
+  `);
+  
+   //hero slider
+   new Swiper(".swiper-container", {
+     loop: false,
+     navigation: {
+       nextEl: ".swiper-button-next",
+       prevEl: ".swiper-button-prev",
+     },
+     pagination: {
+       el: '.swiper-pagination',
+       clickable: true,
+     },
+   })
+ 
+
+
+   //product slider flash deals
+   $('.sale-slider .glider-contain').html(
+    saleData.map(product => `
+      <div class="glider">
+        <div class="sale-product-box">
+          <span class="p-discount"><i class="ri-flashlight-fill"></i><br><b>${product.discount}%</b></span>
+          <div class="sale-img-container">
+            <div class="s-img">
+              <a href="#">
+                <img src="${product.imageSrc}" alt="">
+              </a>
+            </div>
+          </div>
+          <div class="sale-text-box">
+            <div class="p-brand"><span>${product.brand}</span></div>
+            <a href="#" class="p-title">${product.title}</a>
+            <div class="p-price">
+              <span class="current">${product.currentPrice} <span class="before">${product.beforePrice}</span></span>
+              <a href="#" class="p-cart-btn"><i class="ri-shopping-cart-2-line"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    `).join('')
+   );
+  
+   $("#nextBtn").on("click", function () {
+     var nextScroll = $(".glider-contain").scrollLeft() + 250;
+     $(".glider-contain").animate({ scrollLeft: nextScroll }, 400);
+   });
+ 
+   $("#prevBtn").on("click", function () {
+     var prevScroll = $(".glider-contain").scrollLeft() - 250;
+     $(".glider-contain").animate({ scrollLeft: prevScroll }, 400);
+   });
+
+
+ 
+   //image swap new arrivals
+   var imageArray = ["new-arrivals-1.jpg", "new-arrivals-2.jpg"];
+   var currentIndex = 0;
+ 
+   function swapImage() {
+     if (currentIndex < imageArray.length) {
+       $(".arrival-image-swap").attr("src", "./assets/img/" + imageArray[currentIndex]);
+       currentIndex++;
+     } else currentIndex = 0;
+   }
+ 
+   setInterval(swapImage, 900);
+
+
+   
+   //product swiper
+    const itemsHTML = productSideBannerImages.map(imageSrc => `
+     <div class="item">
+       <img src="${imageSrc}" alt="">
+     </div>
+   `).join('');
+    
+   const dotsHTML = productSideBannerImages.map((index) => `
+     <li${index === 0 ? ' class="active"' : ''}></li>
+   `).join('');
+    
+   $('.slider').html(`
+     <div class="list">
+       ${itemsHTML}
+     </div>
+     <div class="buttons">
+       <button id="prev"><</button>
+       <button id="next">></button>
+     </div>
+     <ul class="dots">
+       ${dotsHTML}
+     </ul>
+   `);
+   
+   var sideBannerSlider = $('.slider .list');
+   var items = $('.slider .list .item');
+ 
+   var active = 0;
+
+
+   //grid products
+   var upperProduct = gridProducts.slice(0, 8).map((product) => (
+     `
+      <div class="product-swiper-slide">
+        <div class="sale-product-box">
+          <div class="sale-img-container">
+            <div class="s-img">
+              <a href="#">
+                <img src="${product.imageSrc}" alt="${product.title}">
+              </a>
+            </div>
+          </div>
+          <div class="sale-text-box">
+            <a href="#" class="p-title">${product.title}</a>
+            <div class="p-price">
+              <span class="current">${product.currentPrice} ${product.discount !== '' ? `<span class="before">${product.discount}</span>` : ''}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+  ));
+
+  var lowerProduct = gridProducts.slice(8).map((product) => (
+     `
+      <div class="product-swiper-slide">
+        <div class="sale-product-box">
+          <div class="sale-img-container">
+            <div class="s-img">
+              <a href="#">
+                <img src="${product.imageSrc}" alt="${product.title}">
+              </a>
+            </div>
+          </div>
+          <div class="sale-text-box">
+            <a href="#" class="p-title">${product.title}</a>
+            <div class="p-price">
+              <span class="current">${product.currentPrice} ${product.discount !== '' ? `<span class="before">${product.discount}</span>` : ''}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+  ));
+
+  $(".swiper-containe1 .swiper-wrapper").html(upperProduct.join(''));
+  $(".swiper-containe2 .swiper-wrapper").html(lowerProduct.join(''));
+
+  //move products function
+   $('#next').on("click", function () {
+     active = active + 1 <= items.length - 1 ? active + 1 : 0;
+     reloadSlider();
+   });
+ 
+   $('#prev').on("click", function () {
+     active = active - 1 >= 0 ? active - 1 : items.length - 1;
+     reloadSlider();
+   });
+ 
+   function reloadSlider() {
+     sideBannerSlider.css('left', -items[active].offsetLeft + 'px');
+     $('.slider .dots li.active').removeClass('active');
+     $('.slider .dots li').eq(active).addClass('active');
+   };
+ 
+   $('.slider .dots li').each(function (key) {
+     $(this).on('click', function () {
+       active = key;
+       reloadSlider();
+     });
+   });
+ 
+   $(window).on('resize', function () {
+     reloadSlider();
+     toggleBackToTop();
+   });
+ 
+   var topProduct = $(".swiper-containe1");
+   var botProduct = $(".swiper-containe2");
+ 
+   $(".products .swiper-button-prev").on("click", function () {
+     var scrollAmount = topProduct.scrollLeft() - 250;
+     topProduct.animate({ scrollLeft: scrollAmount }, 400);
+     botProduct.animate({ scrollLeft: scrollAmount }, 400);
+   });
+ 
+   $(".products .swiper-button-next").on("click", function () {
+     var scrollAmount = topProduct.scrollLeft() + 250;
+     topProduct.animate({ scrollLeft: scrollAmount }, 400);
+     botProduct.animate({ scrollLeft: scrollAmount }, 400);
+   });
+ 
+   topProduct.scroll(function () {
+     botProduct.scrollLeft(topProduct.scrollLeft());
+   });
+ 
+   botProduct.scroll(function () {
+     topProduct.scrollLeft(botProduct.scrollLeft());
+   });
+
+
+ });
+ 
---
+++ assets/js/productData.js
@@ -0,0 +1,203 @@
+export const heroSlideData = [
+    {
+      imgSrc: './assets/img/hero/hero-1.jpg',
+      title: 'Save up to 70% On Selected Styles',
+      text: 'Level up your look with these timeless kicks',
+      buttonClass: 'btn-secondary',
+      buttonText: 'Shop Now',
+    },
+    {
+      imgSrc: './assets/img/hero/hero-2.jpg',
+    },
+    {
+      imgSrc: './assets/img/hero/hero-3.jpg',
+      title: 'Nike Precision 6 Color Ways',
+      text: 'New Releases & Iconic Styles',
+      buttonClass: 'btn-primary',
+      buttonText: 'Shop Now',
+    },
+    {
+      imgSrc: './assets/img/hero/hero-4.jpg',
+      title: 'Sperry X Malbon Golf',
+      text: 'Everyday Luxury Essentials',
+      titleColor: 'var(--white)',
+      textColor: 'var(--white)',
+      buttonClass: 'btn-primary',
+      buttonText: 'Shop Now',
+    },
+    {
+      imgSrc: './assets/img/hero/hero-5.jpg',
+    },
+    {
+      imgSrc: './assets/img/hero/hero-6.jpg',
+    },
+];
+
+
+export const saleData = [
+  {
+    discount: -20,
+    brand: 'Nike',
+    title: 'Precision 6 Panda',
+    currentPrice: '₱3,995',
+    beforePrice: '₱4,999',
+    imageSrc: './assets/img/sale/precision-panda.jpg',
+  },
+  {
+    discount: -45,
+    brand: 'Nike',
+    title: 'Jordan 1 High Bred',
+    currentPrice: '₱10,699',
+    beforePrice: '₱18,499',
+    imageSrc: './assets/img/sale/jordan-1-high.jpg',
+  },
+  {
+    discount: -63,
+    brand: 'Adidas',
+    title: 'Yeezy Boost 350 Sesame',
+    currentPrice: '₱6,000',
+    beforePrice: '₱16,000',
+    imageSrc: './assets/img/sale/yeezy-sesame.jpg',
+  },
+  {
+    discount: -83,
+    brand: 'Converse',
+    title: 'Converse High CDG',
+    currentPrice: '₱1,999',
+    beforePrice: '₱11,500',
+    imageSrc: './assets/img/sale/converse-cdg.jpg',
+  },
+  {
+    discount: -20,
+    brand: 'Nike',
+    title: 'Precision 6 Team Red',
+    currentPrice: '₱3,995',
+    beforePrice: '₱4,999',
+    imageSrc: './assets/img/sale/precision-team-red.jpg',
+  },
+  {
+    discount: -25,
+    brand: 'Crocs',
+    title: 'Salehe Tide',
+    currentPrice: '₱8,799',
+    beforePrice: '₱9,950',
+    imageSrc: './assets/img/sale/crocs-tide.jpg',
+  },
+  {
+    discount: -20,
+    brand: 'Nike',
+    title: 'Precision 6 Midnight Navy',
+    currentPrice: '₱3,995',
+    beforePrice: '₱4,999',
+    imageSrc: './assets/img/sale/precision-midnight-navy.jpg',
+  },
+];
+
+export const productSideBannerImages = [
+  './assets/img/product-banner-1.jpg',
+  './assets/img/product-banner-2.jpg',
+  './assets/img/product-banner-3.jpg',
+  './assets/img/product-banner-4.jpg',
+  './assets/img/product-banner-5.jpg',
+];
+
+
+export const gridProducts = [
+  {
+    imageSrc: './assets/img/products/product-1.jpg',
+    title: 'Adidas Gazelle',
+    currentPrice: '₱4,000',
+    discount: '-23%',
+  },
+  {
+    imageSrc: './assets/img/products/product-2.jpg',
+    title: 'Nike Dunk Low',
+    currentPrice: '₱3,000',
+    discount: '-30%',
+  },
+  {
+    imageSrc: './assets/img/products/product-3.jpg',
+    title: 'Jordan 1 High Bred',
+    currentPrice: '₱10,699',
+    discount: '-53%',
+  },
+  {
+    imageSrc: './assets/img/products/product-4.jpg',
+    title: 'Salehe Black Sasquatch',
+    currentPrice: '₱9,499',
+    discount: '',
+  },
+  {
+    imageSrc: './assets/img/products/product-5.jpg',
+    title: 'Converse High CDG',
+    currentPrice: '₱1,999',
+    discount: '-83%',
+  },
+  {
+    imageSrc: './assets/img/products/product-6.jpg',
+    title: 'Yeezy 500 Soft Vision',
+    currentPrice: '₱7,499',
+    discount: '-30%',
+  },
+  {
+    imageSrc: './assets/img/products/product-7.jpg',
+    title: 'Jordan 1 Tatum',
+    currentPrice: '₱6,250',
+    discount: '',
+  },
+  {
+    imageSrc: './assets/img/products/product-8.jpg',
+    title: 'Sperry Gold Cup',
+    currentPrice: '₱2,495',
+    discount: '',
+  },
+  {
+    imageSrc: './assets/img/products/product-9.jpg',
+    title: 'Nike Dunk Low Phantom',
+    currentPrice: '₱3,999',
+    discount: '',
+  },
+  {
+    imageSrc: './assets/img/products/product-10.jpg',
+    title: 'Sperry Goldcup Topsider',
+    currentPrice: '₱2,350',
+    discount: '',
+  },
+  {
+    imageSrc: './assets/img/products/product-11.jpg',
+    title: 'Jordan 4 a Ma Maniere',
+    currentPrice: '₱30,199',
+    discount: '',
+  },
+  {
+    imageSrc: './assets/img/products/product-12.jpg',
+    title: 'Jordan 1 High AMM',
+    currentPrice: '₱17,999',
+    discount: '',
+  },
+  {
+    imageSrc: './assets/img/products/product-13.jpg',
+    title: 'Yeezy Boost 350 V2 Slate',
+    currentPrice: '₱17,999',
+    discount: '-50%',
+  },
+  {
+    imageSrc: './assets/img/products/product-14.jpg',
+    title: 'Crocs X Staple',
+    currentPrice: '₱6,490',
+    discount: '',
+  },
+  {
+    imageSrc: './assets/img/products/product-15.jpg',
+    title: 'Steal Yeezy 700 MNVM',
+    currentPrice: '₱7,499',
+    discount: '-50%',
+  },
+  {
+    imageSrc: './assets/img/products/product-16.jpg',
+    title: 'Jordan 1 High Defiant',
+    currentPrice: '₱3,500',
+    discount: '-65%',
+  },
+];
+
